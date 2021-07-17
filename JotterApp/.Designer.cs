e49721e0a6928ae1325c54e9a8bb4058f48df64e
Indre Macernyte <indremacernyte@gmail.com>
2021-07-17 10:24:14 +0100

    New Files added

---
+++ JotterApp/.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace JotterApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelNote = new System.Windows.Forms.Label();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.labelNoteNumber = new System.Windows.Forms.Label();
+            this.buttonNew = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.textTitle = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textMessage = new System.Windows.Forms.TextBox();
+            this.buttonViewNote = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelNote
+            // 
+            this.labelNote.AutoSize = true;
+            this.labelNote.Location = new System.Drawing.Point(8, 79);
+            this.labelNote.Name = "labelNote";
+            this.labelNote.Size = new System.Drawing.Size(33, 13);
+            this.labelNote.TabIndex = 1;
+            this.labelNote.Text = "Note:";
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Location = new System.Drawing.Point(8, 48);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(30, 13);
+            this.labelTitle.TabIndex = 2;
+            this.labelTitle.Text = "Title:";
+            // 
+            // labelNoteNumber
+            // 
+            this.labelNoteNumber.AutoSize = true;
+            this.labelNoteNumber.Location = new System.Drawing.Point(44, 15);
+            this.labelNoteNumber.Name = "labelNoteNumber";
+            this.labelNoteNumber.Size = new System.Drawing.Size(88, 13);
+            this.labelNoteNumber.TabIndex = 3;
+            this.labelNoteNumber.Text = "You have   notes";
+            // 
+            // buttonNew
+            // 
+            this.buttonNew.Location = new System.Drawing.Point(47, 377);
+            this.buttonNew.Name = "buttonNew";
+            this.buttonNew.Size = new System.Drawing.Size(86, 29);
+            this.buttonNew.TabIndex = 4;
+            this.buttonNew.Text = "New note";
+            this.buttonNew.UseVisualStyleBackColor = true;
+            this.buttonNew.Click += new System.EventHandler(this.buttonNew_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(323, 377);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(86, 29);
+            this.buttonDelete.TabIndex = 5;
+            this.buttonDelete.Text = "Delete Note";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(416, 377);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(86, 29);
+            this.buttonClose.TabIndex = 6;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // textTitle
+            // 
+            this.textTitle.Location = new System.Drawing.Point(47, 45);
+            this.textTitle.Name = "textTitle";
+            this.textTitle.Size = new System.Drawing.Size(124, 20);
+            this.textTitle.TabIndex = 7;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textMessage
+            // 
+            this.textMessage.Location = new System.Drawing.Point(47, 79);
+            this.textMessage.Multiline = true;
+            this.textMessage.Name = "textMessage";
+            this.textMessage.Size = new System.Drawing.Size(248, 278);
+            this.textMessage.TabIndex = 8;
+            // 
+            // buttonViewNote
+            // 
+            this.buttonViewNote.Location = new System.Drawing.Point(231, 377);
+            this.buttonViewNote.Name = "buttonViewNote";
+            this.buttonViewNote.Size = new System.Drawing.Size(86, 29);
+            this.buttonViewNote.TabIndex = 10;
+            this.buttonViewNote.Text = "View Note";
+            this.buttonViewNote.UseVisualStyleBackColor = true;
+            this.buttonViewNote.Click += new System.EventHandler(this.buttonViewNote_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(312, 79);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(190, 278);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(312, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "label1";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(139, 377);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(86, 29);
+            this.buttonSave.TabIndex = 13;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 418);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonViewNote);
+            this.Controls.Add(this.textMessage);
+            this.Controls.Add(this.textTitle);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonNew);
+            this.Controls.Add(this.labelNoteNumber);
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.labelNote);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label labelNote;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label labelNoteNumber;
+        private System.Windows.Forms.Button buttonNew;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.TextBox textTitle;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textMessage;
+        private System.Windows.Forms.Button buttonViewNote;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonSave;
+    }
+}
+
---
+++ JotterApp/frmJotter.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JotterApp
+{
+    public partial class Form1 : Form
+    {
+
+        DataTable table;
+      
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table = new DataTable();
+            table.Columns.Add("Title", typeof(String));
+            table.Columns.Add("Messages", typeof(String));
+
+            dataGridView1.DataSource = table;
+
+            dataGridView1.Columns["Messages"].Visible = false;
+            dataGridView1.Columns["Title"].Width = 187;
+        }
+        private void buttonNew_Click(object sender, EventArgs e)
+        {
+            textTitle.Clear();
+            textMessage.Clear();
+        }
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            table.Rows.Add(textTitle.Text, textMessage.Text);
+            textTitle.Clear();
+            textMessage.Clear();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+
+            table.Rows[index].Delete();
+        }
+        private void buttonViewNote_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+
+            if(index > -1)
+            {
+                textTitle.Text = table.Rows[index]["Title"].ToString();
+                textMessage.Text = table.Rows[index]["Messages"].ToString();
+            }
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    
+        public void incrementNoteLabel()
+        {
+
+        }
+
+      
+    }  
+}
